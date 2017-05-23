cdbeeecf32112e9a8c39ccd88c37ae25a3f57f44
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2017-05-24 02:30:15 +0530

    Format: pep8 guidelines

--- logs.py
+++ logs.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 import psycopg2
 DB_NAME = "news"
 
@@ -5,7 +7,9 @@ DB_NAME = "news"
 query1 = "select title,views from article_view limit 3"
 
 # 2. Who are the most popular article authors of all time?
-query2 = "select authors.name,sum(article_view.views) as views from article_view,authors where authors.id = article_view.author group by authors.name order by views desc"
+query2 = """select authors.name,sum(article_view.views) as views from
+article_view,authors where authors.id = article_view.author
+group by authors.name order by views desc"""
 
 # 3. On which days did more than 1% of requests lead to errors?
 query3 = "select * from error_log_view where \"Percent Error\" > 1"
@@ -15,10 +19,12 @@ query_1_result = dict()
 query_1_result['title'] = "\n1. The 3 most popular articles of all time are:\n"
 
 query_2_result = dict()
-query_2_result['title'] = "\n2. The most popular article authors of all time are:\n"
+query_2_result['title'] = """\n2. The most popular article authors of
+all time are:\n"""
 
 query_3_result = dict()
-query_3_result['title'] = "\n3. Days with more than 1% of request that lead to an error:\n"
+query_3_result['title'] = """\n3. Days with more than 1% of request that
+lead to an error:\n"""
 
 
 # returns query result
@@ -30,15 +36,18 @@ def get_query_result(query):
     db.close()
     return results
 
+
 def print_query_results(query_result):
     print (query_result['title'])
     for result in query_result['results']:
         print ('\t' + str(result[0]) + ' ---> ' + str(result[1]) + ' views')
 
+
 def print_error_query_results(query_result):
     print (query_result['title'])
     for result in query_result['results']:
         print ('\t' + str(result[0]) + ' ---> ' + str(result[1]) + ' %')
+
 
 # stores query result
 query_1_result['results'] = get_query_result(query1)
